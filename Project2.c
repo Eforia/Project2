fbc867145eeaa66603b496c7fa2046fb25525d3e
Gerasimos Giannoutsos <GerasimosGiannoutsos@proton.me>
2024-12-26 10:33:56 +0200

    init of pipe and some framework

--- Project2.c
+++ Project2.c
@@ -11,67 +11,81 @@
 
 
 typedef struct product{
-    char name[MAX_NAME];
+    char description[MAX_NAME];
     int item_count;
     float price;
 } product;
 
 
-product array[TABLE_SIZE] = {
-    {"Milk", 2, 2.99},
-    {"Eggs", 2, 4.50},
-    {"Bread", 2, 1.99},
-    {"Butter", 2, 3.49},
-    {"Cheese", 2, 5.99},
-    {"Apples", 2, 1.89},
-    {"Bananas", 2, 1.29},
-    {"Chicken", 2, 4.99},
-    {"Beef", 2, 6.99},
-    {"Rice", 2, 3.99},
-    {"Pasta", 2, 1.79},
-    {"Tomato Sauce", 2, 2.49},
-    {"Cereal", 2, 3.99},
-    {"Orange Juice", 2, 4.29},
-    {"Coffee", 2, 8.99},
-    {"Tea", 2, 3.50},
-    {"Potatoes", 2, 2.99},
-    {"Onions", 2, 1.89},
-    {"Sugar", 2, 2.79},
-    {"Flour", 2, 2.49}
-};
 
 
-void main (int argc,char **argv)
+
+int main (int argc,char **argv)
 {
+    int fd[2];
+    if (pipe(fd) == -1)
+    {
+        printf("Error regarding pipe creation\n");
+        return 1;
+    }
+
     int ppid[5];
     ppid[0] = fork();
 
-    if (ppid[0] == 0)
+    if (ppid[0] == 1)
     {
-        //parent process
+        close(fd[1]);
+
+        product catalog[TABLE_SIZE] = {
+            {"Milk", 2, 2.99},
+            {"Eggs", 2, 4.50},
+            {"Bread", 2, 1.99},
+            {"Butter", 2, 3.49},
+            {"Cheese", 2, 5.99},
+            {"Apples", 2, 1.89},
+            {"Bananas", 2, 1.29},
+            {"Chicken", 2, 4.99},
+            {"Beef", 2, 6.99},
+            {"Rice", 2, 3.99},
+            {"Pasta", 2, 1.79},
+            {"Tomato Sauce", 2, 2.49},
+            {"Cereal", 2, 3.99},
+            {"Orange Juice", 2, 4.29},
+            {"Coffee", 2, 8.99},
+            {"Tea", 2, 3.50},
+            {"Potatoes", 2, 2.99},
+            {"Onions", 2, 1.89},
+            {"Sugar", 2, 2.79},
+            {"Flour", 2, 2.49}
+        };
+
+        for(int i = 0; i < TABLE_SIZE; i++)
+        {
+            printf("%c priced at: %f\n", catalog[i].description, catalog[i].price);
+        }
 
         ppid[1] = fork();
 
-        if(ppid[1] == 0)
+        if(ppid[1] == 1)
         {
             //parent process
 
             ppid[2] = fork();
-            if(ppid[2] == 0)
+            if(ppid[2] == 1)
             {
                 //parent process
 
                 ppid[3] = fork();
-                if(ppid[3] == 0)
+                if(ppid[3] == 1)
                 {
                     //parent process
 
                     ppid[4] = fork();
-                    if(ppid[4] == 0)
+                    if(ppid[4] == 1)
                     {
                         //parent process
                     }
-                    else if (ppid[4] == 1)
+                    else if (ppid[4] == 0)
                     {
                         //child process
                     }
@@ -81,7 +95,7 @@ void main (int argc,char **argv)
                     }
                     
                 }
-                else if (ppid[3] == 1)
+                else if (ppid[3] == 0)
                 {
                     //child process
                 }
@@ -91,7 +105,7 @@ void main (int argc,char **argv)
                 }
                 
             }
-            else if (ppid[2] == 1)
+            else if (ppid[2] == 0)
             {
                 //child process
             }
@@ -100,7 +114,7 @@ void main (int argc,char **argv)
                 return -1;
             }
         }
-        else if (ppid[1] == 1)
+        else if (ppid[1] == 0)
         {
             //child process
         }
@@ -109,7 +123,7 @@ void main (int argc,char **argv)
             return -1;
         }
     }
-    else if (ppid[0] == 1)
+    else if (ppid[0] == 0)
     {
         //child process
     }
@@ -117,5 +131,8 @@ void main (int argc,char **argv)
     {
         return -1;
     }
+
+
+    return 0;
     
 }
